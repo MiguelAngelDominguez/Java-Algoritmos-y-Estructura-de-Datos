96754107099d8f91ba0e2c3e3bc365b13259abe2
pana-miguel <miguelangel.dominguez@unheval.pe>
2023-09-02 21:57:34 -0500

    Se modifico el codigo, se crearon 4 funciones anidadas una dentro de otra, que verificar el caracter ingresado

--- src/ejercicio_2.java
+++ src/ejercicio_2.java
@@ -2,31 +2,47 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 public class ejercicio_2 {
-    public static boolean esMayuscula(String s) {
-        return s.equals(s.toUpperCase());
+
+    public static String verficarCrt( String s){
+        return esNumero(s);
     }
 
-    public static boolean esMinuscula(String s) {
-        return s.equals(s.toLowerCase());
-    }
-
-    public static  boolean esNumero( String n){
+    public static  String esNumero( String n){
         try{
-            int nuevoInt = Integer.getInteger(n);
-            return true;
+            int nuevoInt = Integer.parseInt(n);
+            return "un Numero";
         }catch(Exception e ){
-            return false;
+            return esMayuscula(n);
         }
 
     }
+    public static String esMayuscula(String s) {
+        if (s.equals(s.toUpperCase())) {
+            return "una Letra Mayuscula";
+        }
+        return esMinuscula(s);
+    }
+
+    public static String esMinuscula(String s) {
+        if (s.equals(s.toLowerCase())){
+            return "una Letra Minuscula";
+        }
+        return "";
+    }
+
+
     public static void main(String[] args) throws IOException{
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("ingrese un letra: ");
-        String letra = reader.readLine();
-        System.out.println("Es minuscula? "+esMinuscula(letra));
-        System.out.println("Es mayuscula? " + esMayuscula(letra));
-        System.out.print("ingrese un Numero: ");
-        String numero = reader.readLine();
+        System.out.print("ingrese un caracter: ");
+        String crt = reader.readLine();
+
+        try{
+            String valorCrt = verficarCrt(crt);
+            System.out.println("El caracter ingresado es "+valorCrt);
+
+        }catch (Exception e){
+            System.out.println("El caracter ingresado es desconocido");
+        }
 
     }
 }
