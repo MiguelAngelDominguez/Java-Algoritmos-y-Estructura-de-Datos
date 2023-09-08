0dd55412781805d0346c495f22a03e0144ec1e97
MiguelDominguez <miguelangel.dominguez@unheval.pe>
2023-09-08 07:40:36 -0500

    codigo del ejercicio 1 de la clase 2, se desarrolla un programa para promediar notas usando bucles

--- src/clase2_ejercicio_1.java
+++ src/clase2_ejercicio_1.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class clase2_ejercicio_1 {
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class clase2_ejercicio_1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int cntNOta = 0 ;
+        float promedioNotas = 0;
+        String  nota = "0";
+        while ( cntNOta<=7 ){
+            do {
+                try{
+                    System.out.print(" - Ingrese una Nota: ");
+                    nota = reader.readLine();
+                    Integer.parseInt(nota);
+                    System.out.println(nota);
+                    if (nota.equals("")){
+                        System.out.println("No se ingreso la nota ");
+                    }
+                }catch (Exception e){
+                    nota="";
+                    System.out.println("La nota ingresa no es un numero");
+                }
+            }while (nota.equals(""));
+            promedioNotas  += Integer.parseInt(nota);
+            cntNOta++;
+        }
+
+        System.out.println(promedioNotas/7);
+
+    }
 }
