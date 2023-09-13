689cd66cb9e822fa0c7775d583ab421685313de5
pana-miguel <miguelangel.dominguez@unheval.pe>
2023-09-13 09:31:18 -0500

    Se agrego las un pequeño delay al algoritmo que simula un reloj

--- src/clase2_ejercicio_3.java
+++ src/clase2_ejercicio_3.java
@@ -1,7 +1,5 @@
 import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
 public class clase2_ejercicio_3 {
     public static void main(String[] args) throws IOException{
         while(true){
