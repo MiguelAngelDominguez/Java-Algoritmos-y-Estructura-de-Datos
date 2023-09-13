03b98718f4e87599b9a3cd55aa36fbea25ae98c5
MiguelDominguez <miguelangel.dominguez@unheval.pe>
2023-09-12 20:47:18 -0500

    Algoritmo que simula ser un reloj

--- src/clase2_ejercicio_3.java
+++ src/clase2_ejercicio_3.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class clase2_ejercicio_3 {
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class clase2_ejercicio_3 {
+    public static void main(String[] args) throws IOException{
+        while(true){
+            for (int h=0; h <24; h++){
+                System.out.println("se cumplio horas");
+                for (int m=0; m<60; m++){
+                    System.out.println("se cumplio minuto");
+                    for (int s=0; s<60; s++){
+                        System.out.println(h+":"+m+":"+s);
+                    }
+                }
+            }
+        }
+        // falta implementar un pequeño delay para simular con exito un reloj
+    }
 }
