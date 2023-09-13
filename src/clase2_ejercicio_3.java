36a66af9c624c5b66b697c4e684be710b637dac6
pana-miguel <miguelangel.dominguez@unheval.pe>
2023-09-13 09:31:14 -0500

    Se agrego las un pequeño delay al algoritmo que simula un reloj

--- src/clase2_ejercicio_3.java
+++ src/clase2_ejercicio_3.java
@@ -10,11 +10,17 @@ public class clase2_ejercicio_3 {
                 for (int m=0; m<60; m++){
                     System.out.println("se cumplio minuto");
                     for (int s=0; s<60; s++){
-                        System.out.println(h+":"+m+":"+s);
+                        try {
+                            //
+                            Thread.sleep(1000);
+                            System.out.println(h+":"+m+":"+s);
+                        } catch (Exception e) {
+                            System.out.println(e);
+                        }
+
                     }
                 }
             }
         }
-        // falta implementar un pequeño delay para simular con exito un reloj
     }
 }
