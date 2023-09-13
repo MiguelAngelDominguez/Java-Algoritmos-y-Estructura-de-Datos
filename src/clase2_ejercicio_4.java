2e4cf3e033bf4aea8fcd357859182126254e50c3
MiguelDominguez <miguelangel.dominguez@unheval.pe>
2023-09-12 20:49:43 -0500

    Se comenzo con el desarrollo para el algoritmo para imprimir los numeros de fibonacci

--- src/clase2_ejercicio_4.java
+++ src/clase2_ejercicio_4.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class clase2_ejercicio_4 {
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class clase2_ejercicio_4 {
+    // 1 1 2 3 5 8 13 21 34
+    static int ini = 1;
+    public static void fiboNacci(int e, int j){
+        System.out.print(ini+" ");
+        ini = e;
+        e = ini + e;
+        if(j==0){
+            return;
+        }
+        j--;
+        fiboNacci(e,j);
+    }
+    public static void main(String[] args) throws IOException{
+        fiboNacci(ini,12);
+    }
 }
