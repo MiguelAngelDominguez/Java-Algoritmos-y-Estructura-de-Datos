ec6f782e5f8e891fef5930b4e8f59f398fe71802
MiguelDominguez <miguelangel.dominguez@unheval.pe>
2023-09-16 08:45:40 -0500

    En el archivo main cree los objetos de la clase personajes

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        Personaje Tank = new Personaje(1,"Tank","Lanzar Rocas",6000,"El tank GAA","negro",4,"Tankear","macho");
+        Personaje Hunter = new Personaje(2,"Hunter","Atrapar a un Oponente",400,"El Hunter GAA","verde",1.5,"Atanking","macho");
+
+        System.out.println(Tank.toString());
+        System.out.println(Hunter.toString());
     }
 }
