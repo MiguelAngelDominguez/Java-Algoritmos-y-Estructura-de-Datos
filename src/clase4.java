3a40c81974d89685070efc522d1b0e4cd32a7702
MiguelDominguez <miguelangel.dominguez@unheval.pe>
2023-09-16 08:42:31 -0500

    En la clase 4 se vio POO

---
+++ src/clase4.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class clase4 {
+    public static void main (String[] args) throws IOException{
+        // Clases .- las clases son las mas comunes y basica en java, los clases tratan de representar los cosas de la vida real
+        // las clases tiene atributos y metodos que representan a un objeto.
+        // A partir de una clase podemos crear un variedad de clases.
+        //
+    }
+}
