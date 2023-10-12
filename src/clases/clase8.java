b3b6ea6732bcc3a8624fa93c4e954bc6649aa40e
MiguelDominguez <miguelangel.dominguez@unheval.pe>
2023-10-12 12:28:12 -0500

    Archivo de la clase 8, tema manejo de archivos

---
+++ src/clases/clase8.java
@@ -0,0 +1,16 @@
+package clases;
+
+import java.io.*;
+
+public class clase8 {
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    public static void main (String[] args) throws IOException {
+        /*
+        Procesamiento de Texto en ficheros
+         */
+        // PrintWriter salida
+        System.out.println("Ingrese un cadena para guardar en un archivo txt: ");
+        String texto = reader.readLine();
+        //FileWriter fw = new FileWriter();
+    }
+}
